92aeef90b4fd676e51219678462760993cc0840b
lesnaya1shelupon <tookdd55@gmail.com>
2022-05-04 02:07:24 +0300

    2 ind zad 15 var

---
+++ 2 in zad 15 var.py
@@ -0,0 +1,17 @@
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+i = complex
+
+d = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % d)
+if d == 0:
+    x = -b / (2 * a)
+    print('x = ' + str(x))
+else:
+    x1 = (-b + d ** 0.5) / (2 * a)
+    x2 = (-b - d ** 0.5) / (2 * a)
+    print('x₁ = ' + str(x1))
+    print('x₂ = ' + str(x2))
