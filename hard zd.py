532b1028ab7becb702e13d251e91de5acc579bed
lesnaya1shelupon <tookdd55@gmail.com>
2022-05-08 01:23:41 +0300

    hard zad

---
+++ hard zd.py
@@ -0,0 +1,22 @@
+import math
+import sys
+EPS = 1e-10
+
+if __name__ == '__main__':
+    x = float(input("Value of x? "))
+    n = int(input("Value of n = "))
+    if x == 0:
+      print("Illegal value of x", file=sys.stderr)
+      exit(1)
+
+
+a = 1 / math.factorial(n + 1)
+S, k = a, 0
+
+
+while math.fabs(a) > EPS:
+    a *= (x ** 2) / ((4 * (k ** 2)) + (4 * k * n) + (8 * k) + (4 * n) + 4)
+    S += a
+    k += 1
+
+print(f"Ei({x}) = {((x / 2) ** n) * S}")
